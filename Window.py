1cb54ae8360ce75dda3ecf13574721b00349db2c
Sourcesketch <hammad@sourcesketch.com>
2023-04-12 17:56:29 +0500

    Window funcation hello

--- Window.py
+++ Window.py
@@ -1,7 +1,34 @@
 import tkinter as tk
+from tkinter import ttk
+
+def button_fun():
+    output_string.set('Hello World')
 
 # create a window
 window=tk.Tk()
+window.title('Window and Widgets')
+window.geometry('800x600')
+
+#create widgets
+label=tk.Text(master = window)
+label.pack()
+
+# ttk widgets
+text=ttk.Label(master=window, text= 'This is text')
+text.pack()
+
+# ttk entry
+entry=ttk.Entry(master=window)
+entry.pack()
+
+# output
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, text='Output', font='Poppin 14', textvariable=output_string)
+output_label.pack(pady=10)
+
+# ttk button
+button=ttk.Button(master=window, text='Click here', command = button_fun)
+button.pack()
 
 # run
 window.mainloop()
