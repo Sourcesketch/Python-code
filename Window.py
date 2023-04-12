696331b742427393f0b631e4ba37827e4d6ba471
Sourcesketch <hammad@sourcesketch.com>
2023-04-12 17:26:45 +0500

    First commit

---
+++ Window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+# create a window
+window=tk.Tk()
+
+# run
+window.mainloop()
---
+++ converter.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+# from tkinter import ttk
+import ttkbootstrap as ttk
+
+def convert():
+    mile_input=entry_int.get()
+    km_output=mile_input * 1.61
+    output_string.set(km_output)
+
+# window
+window=ttk.Window(themename='journal')
+window.title('Demo')
+window.geometry('450x150')
+
+# title
+title_label = ttk.Label(master=window, text='Miles (m) to Kilometer (km)', font='Poppin 18 bold')
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master=window)
+entry_int = tk.IntVar()
+entry= ttk.Entry(master=input_frame, textvariable = entry_int)
+button= ttk.Button(master=input_frame, text='convert', command= convert)
+entry.pack(side='left', padx=10,)
+button.pack(side='left')
+input_frame.pack(pady=10)
+
+# output
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, text='Output', font='Poppin 14', textvariable=output_string)
+output_label.pack(pady=10)
+
+# run 
+window.mainloop()
